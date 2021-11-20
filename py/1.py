153b212609c098a77873a52fe9a0f502863e15e7
nejiklol <46971826+nejiklol@users.noreply.github.com>
2021-11-20 22:37:05 +0300

    Update 1.py

--- py/1.py
+++ py/1.py
@@ -1,1 +1,31 @@
-dccsdc
+class CicleBuff(object):
+    
+    def __init__(self,Max):
+        if type(Max).__name__=='int':
+            self.Max = Max
+            Array = [];
+        pass
+
+    def add(x):
+        if type(x).__name__=='int':
+            if len(Array)==Max:
+                Array[Max]=0
+                i = Max
+                while i != Max:
+                    
+            
+        pass
+
+    def get():
+        self.Max = Max
+        pass
+
+    def print():
+        self.Max = Max
+        pass
+
+
+if __name__ == "__main__":
+    Buffer = CicleBuff(5)
+    print(Buffer.Max)
+    
