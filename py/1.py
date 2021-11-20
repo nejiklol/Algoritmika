6077dd6e33c06344c814f19d28fef616ad80db86
nejiklol <46971826+nejiklol@users.noreply.github.com>
2021-11-21 02:19:40 +0300

    Update 1.py

--- py/1.py
+++ py/1.py
@@ -3,25 +3,33 @@ class CicleBuff(object):
     def __init__(self,Max):
         if type(Max).__name__=='int':
             self.Max = Max
-            Array = [];
+            self.Array = [];
         pass
 
-    def add(x):
+    def add(self,x):
         if type(x).__name__=='int':
-            if len(Array)==Max:
-                Array[Max]=0
-                i = Max
-                while i != Max:
-                    
-            
+            if len(self.Array)==self.Max:
+                self.Array[self.Max-1]=0
+                i = self.Max
+                i = i - 1
+                while i != 0:
+                    self.Array[i] = self.Array[i-1]
+                    i = i - 1
+                self.Array[0] = x
+                
+            else:
+                self.Array.insert(len(self.Array),x)
+        else:
+            print('need Int')
         pass
 
-    def get():
-        self.Max = Max
+    def get(self):
+        print(self.Array[self.Max-1])
+        self.Array=self.Array[:-1]
         pass
 
-    def print():
-        self.Max = Max
+    def print(self):
+        print(self.Array)
         pass
 
 
@@ -29,3 +37,10 @@ if __name__ == "__main__":
     Buffer = CicleBuff(5)
     print(Buffer.Max)
     
+    i = 10
+    while i != 0:
+        Buffer.add(i)
+        i = i - 1
+
+    Buffer.print()
+    Buffer.get()
